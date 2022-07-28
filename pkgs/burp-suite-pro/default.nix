dd73bf0cb3a5aa59436c73a4418cf7ccfc9bec1e
stoek <stanvandervleuten95@gmail.com>
2022-07-28 08:23:38 +0200

    flakeupdate

--- pkgs/burp-suite-pro/default.nix
+++ pkgs/burp-suite-pro/default.nix
@@ -7,7 +7,7 @@ stdenv.mkDerivation rec {
   src = fetchurl {
     name = "burp_pro.jar";
     urls = [
-      "https://portswigger-cdn.net/burp/releases/download?product=pro&Version=2022.7.1&type=Jar"
+      "https://portswigger-cdn.net/burp/releases/download?product=pro&version=${version}&type=Jar"
     ];
     sha256 = "sha256-svVOafvp8Kv0wp8HduLZmxUo7Hz7zOP/bk3DdhH1zO0=";
   };
