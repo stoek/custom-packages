b42c151a4b0d3040134576ff8df313ae7efb59ff
stoek <stanvandervleuten95@gmail.com>
2023-02-28 12:01:33 +0100

    obsidian

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -3,4 +3,5 @@ with pkgs;
 {
   #List all packages you want to put in the package overlay in here.
   burp-pro = callPackage ./burp-pro {};
+  obsidian-upstream = callPackage ./obsidian-upstream {};
 }
